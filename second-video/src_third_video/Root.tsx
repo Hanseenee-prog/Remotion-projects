d6375060d3042a5d052255b141f51549ba7448e2
Hanon Emmanuel <hansonemma398@gmail.com>
2026-03-24 20:29:39 +0100

    Moved the Scenes into another folder, so I can make a new one

---
+++ second-video/src_third_video/Root.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+import { Composition } from "remotion";
+import { Reel, TOTAL_FRAMES } from "./Reel";
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <Composition
+      id="RocketReel"
+      component={Reel}
+      durationInFrames={TOTAL_FRAMES}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+  );
+};
